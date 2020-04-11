2e3b4a277c699c0b4c1163d6f7d261f7346db47c
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-04-10 21:51:01 -0400

    practicing writing basic api

---
+++ api/users/data.js
@@ -0,0 +1,96 @@
+module.exports = {
+  getUser,
+  addUser,
+  deleteUser,
+  patchUser,
+}
+
+const db = {
+  users: [
+    {
+      id: 1,
+      name: "Jane Doe",
+      bio: "Not Tarzan's Wife, another Jane",
+    },
+  ],
+  top_users: [],
+}
+
+function getIndex(id) {
+  return db.users.findIndex((u) => u.id === Number(id))
+}
+
+function getUser(id) {
+  if (id) {
+    const index = getIndex(id)
+    return db.users[index]
+  } else {
+    return db
+  }
+}
+
+function addUser(user) {
+  const newUser = { ...user, id: db.users.length + 1 }
+  db.users.push(newUser)
+
+  return newUser
+}
+
+function deleteUser(id) {
+  // find index of user with id
+  const index = getIndex(id)
+  const deleted = db.users.splice(index, 1)
+
+  return { deleted }
+}
+
+function patchUser(patch) {
+  switch (patch.op) {
+    case "add":
+      return patchAdd(patch)
+
+    case "replace":
+
+    case "remove":
+
+    case "move":
+
+    case "copy":
+
+    case "test":
+
+    default:
+      return db.users
+  }
+}
+
+function patchAdd(patch) {
+  const { table, index } = parsePath(patch.path)
+
+  if (index >= 0) {
+    db[table].splice(index, 0, patch.value)
+  } else {
+    db[table].push(patch.value)
+  }
+
+  return db[table]
+}
+
+function parsePath(path) {
+  const params = path.split("/").slice(1)
+  let table, index, key
+
+  if (params.length >= 1) {
+    table = params[0]
+
+    if (typeof Number(params[1]) === "number") {
+      // param is a number
+      index = Number(params[1])
+    } else {
+      // param is a string
+      key = params[1]
+    }
+  }
+
+  return { table, index, key }
+}
---
+++ index.js
@@ -0,0 +1,65 @@
+const db = require("./api/users/data")
+const express = require("express")
+const server = express()
+
+server.use(express.json())
+
+server.get("/", (req, res) => {
+  res.json({ message: "api running" })
+})
+
+server.get("/users", (req, res) => {
+  const users = db.getUser()
+  if (users) return res.status(200).json(users)
+
+  if (!users)
+    return res.status(400).json({ error: "Users could not be retrieved" })
+})
+
+server.get("/users/:id", (req, res) => {
+  const id = req.params.id
+
+  const user = db.getUser(id)
+
+  if (user) {
+    return res.status(200).json(user)
+  } else {
+    return res
+      .status(400)
+      .json({ error: "The user with the specified ID does not exist." })
+  }
+})
+
+server.post("/users", (req, res) => {
+  const newUser = req.body
+
+  if (newUser.name && newUser.bio) {
+    const user = db.addUser(newUser)
+    return res.status(201).json(user)
+  } else {
+    return res
+      .status(400)
+      .json({ error: "Please provide name and bio for the user." })
+  }
+})
+
+server.patch("/users", (req, res) => {
+  const patched = db.patchUser(req.body)
+
+  if (patched) {
+    res.status(204).json(patched)
+  } else {
+    res.status(400).json({ error: "Could not patch user" })
+  }
+})
+
+server.delete("/users/:id", (req, res) => {
+  const id = req.params.id
+  const deleted = db.deleteUser(id)
+
+  return res.status(200).json(deleted)
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`server running on ${PORT}`))
